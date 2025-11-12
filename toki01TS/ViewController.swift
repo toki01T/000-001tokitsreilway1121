30fc09863650a9a7175c18c491806c416478bc98
toki01T <toki1457@gmail.com>
2025-11-12 14:20:09 +0900

    Initial Commit

---
+++ toki01TS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  toki01TS
+//
+//  Created by shakehands on 2025/11/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
